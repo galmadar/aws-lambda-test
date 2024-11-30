71647fb708109620e8e62e0e6ae32135dd25e812
Gal Madar <madargal@gmail.com>
2024-12-01 01:01:32 +0200

    added event get body

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import sys
 from dotenv import load_dotenv
-from http_to_files import download_photo
+import json
+from upload_to_instagram import upload_post
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -10,8 +11,24 @@ def handler(event, context):
     print({"event": event, "context": context})
 
     image_url = "https://v5.airtableusercontent.com/v3/u/35/35/1733011200000/NqkUWIFWczhAiDYF8nYL-Q/nsdzwJtaSS6EgPFyPcfi6NxlQ5vJqdjGNizBjp5dWi7G_JvCYD4iu-ADTl48sVpj6aE60HTFraMj40JShUvJBNG6DYIVIQMMwYFcVO6aUyF1UjzQSWCxc2o4jEpf_9KgknpF5PzO8tlmSf89sY4Mvw/_PPYdmcKz23urDXG6fhc2HHcR33WW9rc4BsXAGW2kUE"
-    save_path = download_photo(image_url)
-    print(f"image saved in {save_path}")
+    # save_path = download_photo(image_url)
+    # print(f"image saved in {save_path}")
+
+    body = event.get('body')
+    if not body:
+        raise ValueError("Request body is missing.")
+    
+    # Assume the body is JSON and parse it
+    body_json = json.loads(body)
+    
+    # Extract the 'Generated Post' argument
+    generated_post = body_json.get('Generated Post')
+    if generated_post is None:
+        raise ValueError("'Generated Post' argument is missing.")
+    
+
+    print("about to upload post!")
+    upload_post(image_url, generated_post)
 
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
 
